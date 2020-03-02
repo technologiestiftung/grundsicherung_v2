b588f2eacee06e11efcf78a7b9df4aed488af50b
julizet <juliazet@gmx.de>
2020-03-02 15:00:16 +0100

    inital commit

---
+++ src/js/back2top.js
@@ -0,0 +1,20 @@
+export function scroll2top() {
+  // ===== Scroll to Top ====
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 200) {
+      // If page is scrolled more than 200px
+      $(".scrollToTop").fadeIn(200); // Fade in the arrow
+    } else {
+      $(".scrollToTop").fadeOut(200); // Else fade out the arrow
+    }
+  });
+  $(".scrollToTop").click(function() {
+    // When arrow is clicked
+    $("body,html").animate(
+      {
+        scrollTop: 0 // Scroll to top of body
+      },
+      500
+    );
+  });
+}
---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import "../css/index.scss";
+import { map } from "./map";
+import { linechart } from "./linechart";
+import { timelapse } from "./timelapse";
+import { counter } from "./counter";
+import { scroll2top } from "./back2top";
+
+map();
+linechart();
+timelapse();
+counter();
+scroll2top();
