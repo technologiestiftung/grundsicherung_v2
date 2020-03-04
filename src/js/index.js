d5a14236ae1ce2f4ee4b6b0e65781e07abf41ba8
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-03-04 13:45:56 +0100

    Adds core-js and fetch polyfill remove documentContentLoaded Listeners in favour of only one in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+import "whatwg-fetch";
+import "core-js/stable";
+// import "regenerator-runtime/runtime";
 import "../css/index.scss";
 import { map } from "./map";
 import { linechart } from "./linechart";
@@ -5,8 +8,10 @@ import { timelapse } from "./timelapse";
 import { counter } from "./counter";
 import { scroll2top } from "./back2top";
 
-map();
-linechart();
-timelapse();
-counter();
-scroll2top();
+document.addEventListener("DOMContentLoaded", function() {
+  map();
+  linechart();
+  timelapse();
+  counter();
+  scroll2top();
+});
